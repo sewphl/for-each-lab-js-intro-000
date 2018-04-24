db51b5896fa8abf4b42519ab21b543fa855b3edc
sharon <sharonoptsout@gmail.com>
2018-04-24 22:50:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function iterativeLog(array) {
+  array.forEach((element,index) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  var array = ["peanutbutter","coffee"];
+  array.forEach(callback);
+  return(array);
+}
+
+function doToArray(array,callback) {
+  array.forEach(callback);
+}
