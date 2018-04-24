888cadeef5b8b955da7faf5a56de40a77402975e
sharon <sharonoptsout@gmail.com>
2018-04-24 22:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/*
 function iterativeLog(array) {
   array.forEach((element,index,array) => {
     console.log(`${index}: ${element}`);
   });
 }
+*/
 
 function iterate(callback) {
   var array = ["peanutbutter"];
